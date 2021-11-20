ff95fb688e62ce813ea1c2216b2f2444ce49f0d5
Suraj Magdum <magdumsuraj07@gmail.com>
2021-11-20 17:02:58 +0530

    Added functionality for deleting a node from BST in BST data-structure

--- datastructures/tree/BinarySearchTree.py
+++ datastructures/tree/BinarySearchTree.py
@@ -18,17 +18,47 @@ def insert(root, val):
     return root
 
 
+def delete(root, key):
+    """
+    Deletes the node with value equal to key from given BST
+    """
+    if (root is None):
+        return root
+    if (key < root.data):
+        # Search in left subtree
+        root.left = delete(root.left, key)
+    elif (key > root.data):
+        # Search in right subtree
+        root.right = delete(root.right, key)
+    else:
+        # key == data of current node
+        # Case 1/2 : Node to be deleted has 0/1 child
+        if (root.left is None):
+            temp = root.right
+            root = None
+            return temp
+        elif (root.right is None):
+            temp = root.left
+            root = None
+            return temp
+        else:
+            # Case 3 : Node to be deleted has 2 childs
+            temp = findSuccesor(root.right)
+            root.data = temp.data
+            root.right = delete(root.right, temp.data)
+    return root
+
+
+def findSuccesor(root):
+    curr = root
+    while (curr.left is not None):
+        curr = curr.left
+    return curr
+
+
 def inorderPrint(root):
     if (root is None):
         return
     inorderPrint(root.left)
     print(root.data, end=" ")
     inorderPrint(root.right)
-
-
-if __name__ == '__main__':
-    root = Node(10)
-    root = insert(root, 2)
-    root = insert(root, 3)
-    root = insert(root, 7)
-    inorderPrint(root)
