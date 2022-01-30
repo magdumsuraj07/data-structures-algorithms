6083b68e4bed20e3d5efc8eb53f7fa0016ce5c96
Suraj Magdum <magdumsuraj07@gmail.com>
2022-01-30 22:46:00 +0530

    Added questions on strings

---
+++ questions/string/48_palindrome-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, S):
+        left = 0
+        right = len(S)-1
+
+        while (left < right):
+            if (S[left] != S[right]):
+                return 0
+            left += 1
+            right -= 1
+        return 1
