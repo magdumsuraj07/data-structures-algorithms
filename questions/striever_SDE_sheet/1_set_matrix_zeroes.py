b92ee34886292350063f93af39bff8b25af0798f
Suraj Magdum <magdumsuraj07@gmail.com>
2022-03-04 20:24:54 +0530

    Added question-1 and question-2

---
+++ questions/striever_SDE_sheet/1_set_matrix_zeroes.py
@@ -0,0 +1,31 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        R = len(matrix)
+        C = len(matrix[0])
+        is_col = False
+
+        for i in range(R):
+            if (matrix[i][0] == 0):
+                is_col = True
+            for j in range(1, C):
+                if (matrix[i][j] == 0):
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(1, R):
+            for j in range(1, C):
+                if (matrix[i][0] == 0 or matrix[0][j] == 0):
+                    matrix[i][j] = 0
+
+        # Check if first row needs to be set to zero
+        if (matrix[0][0] == 0):
+            for j in range(C):
+                matrix[0][j] = 0
+
+        # Check if first column needs to be set to zero
+        if (is_col):
+            for i in range(R):
+                matrix[i][0] = 0
---
+++ questions/striever_SDE_sheet/2_pascals_triangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+        prev = []
+        for i in range(numRows):
+            row = []
+            for j in range(i+1):
+                if (j == 0 or j == i):
+                    row.append(1)
+                else:
+                    row.append(prev[j-1] + prev[j])
+            prev = row
+            res.append(row)
+
+        return res
